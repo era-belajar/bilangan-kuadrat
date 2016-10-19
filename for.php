8d88d9487681ec385d03d49acca1652af091233d
Marfi Saputra <marfisaputra14@gmail.com>
2016-10-19 09:27:53 +0700

    Add files via upload
    Menghitung bilangan kuadrat dengan javascript

---
+++ for.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>First JSON </title>
+</head>
+<body>
+
+<p id="data"></p>
+<script>
+document.writeln("<H2>Menentukan Bilangan Kuadrat </h2>");
+document.writeln("--------------------------------------");
+	for (i=1; i<=10; i++)
+	{
+		x=i*i;
+		document.writeln("<br>");
+		document.write("Angka : " + i + " Kuadrat :  " + x + "<BR>");
+		document.writeln("</br>");
+		
+	}
+document.writeln("--------------------------------------");
+
+</script>
+</body>
+</html>
